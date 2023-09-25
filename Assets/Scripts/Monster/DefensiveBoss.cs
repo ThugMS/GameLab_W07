0f01de78b494af42a93431bcfe521f92f49e84ea
MinseokLee <leems041625@gmail.com>
2023-09-26 00:27:19 +0900

    [Feat] : 보스 움직임 구현

--- Assets/Scripts/Monster/DefensiveBoss.cs
+++ Assets/Scripts/Monster/DefensiveBoss.cs
@@ -1,3 +1,4 @@
+using DG.Tweening;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
@@ -11,21 +12,41 @@ public class DefensiveBoss : MonsterBase
     #region PrivateVariables
     [Header("Shiled")]
     [SerializeField] private float m_turnSpeed;
+
+    [Header("Move")]
+    [SerializeField] private float m_moveDis;
+    [SerializeField] private bool m_canMove = true;
+    [SerializeField] private float m_moveTime = 1.0f;
+    [SerializeField] private float m_moveCooldown;
+    [SerializeField] private AnimationCurve m_moveEase;
     #endregion
 
     #region PublicMethod
     private void Update()
     {
         RotateShiled();
+        CheckMove();
     }
 
     protected override void Move()
     {
-        
+        GetTargetDirection();
+        Vector3 movePos;
+
+        if (m_moveDis > (m_player.transform.position - transform.position).magnitude)
+            movePos = m_player.transform.position;
+
+        else
+            movePos = transform.position + m_targetDirection * m_moveDis;
+
+        transform.DOMove(movePos, m_moveTime - 0.3f).SetEase(m_moveEase);
     }
     #endregion
 
     #region PrivateMethod
+    /// <summary>
+    /// 플레이어 방향으로 실드 돌려준다.
+    /// </summary>
     private void RotateShiled()
     {
         GetTargetDirection();
@@ -34,6 +55,31 @@ public class DefensiveBoss : MonsterBase
         Quaternion targetRotation = Quaternion.Euler(0, 0, angle);
 
         m_shiledRotation.rotation = Quaternion.Lerp(m_shiledRotation.rotation, targetRotation, m_turnSpeed);
+    }
+
+    /// <summary>
+    /// 움직일 수 있는지 여부를 체크하고 움직일 수 있다면 움직이게 합니다.
+    /// </summary>
+    private void CheckMove()
+    {
+        if (m_canMove)
+        {
+            Move();
+            m_moveCooldown = m_moveTime;
+            m_canMove = false;
+        }
+        else
+        {
+            m_moveCooldown -= Time.deltaTime;
+
+            if (m_moveCooldown <= 0)
+                m_canMove = true;
+        }
+    }
+
+    private void Charge()
+    {
+
     }
     #endregion
 }
