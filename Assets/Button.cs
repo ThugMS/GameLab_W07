f469f59e175da1e77a1842481bcfae43e1ad287d
Minkyung <kyoongeeee@gmail.com>
2023-09-27 09:09:02 +0900

    [Feat] 보스전 몬스터 생성

--- Assets/Button.cs
+++ Assets/Button.cs
@@ -37,7 +37,6 @@ public class Button : MonoBehaviour
     {
         MapSpawinTrigger.instance.m_stageNum += 1;
         MapSpawinTrigger.instance.SpawnPlayer();
-        MapSpawinTrigger.instance.tutorialTileTwo.SpawnMonsters();
     }
 
     #endregion
