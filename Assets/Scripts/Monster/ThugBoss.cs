ddd87dace5944a0f257e129e2570cb9490c238dc
Minkyung <kyoongeeee@gmail.com>
2023-10-04 13:40:05 +0900

    [Feat] 보스 HP바 연결

--- Assets/Scripts/Monster/ThugBoss.cs
+++ Assets/Scripts/Monster/ThugBoss.cs
@@ -9,6 +9,8 @@ public class ThugBoss : MonsterBase
     public int m_phase = 1;
     #endregion
     #region PrivateVariables
+    private ThugBossPhase1 thugBossPhase1;
+    private ThugBossPhase2 thugBossPhase2;
     #endregion
     #region PublicMethod
 
@@ -19,6 +21,27 @@ public class ThugBoss : MonsterBase
             instance = this;
         }
     }
+
+    protected override void Start()
+    {
+        
+        TryGetComponent<ThugBossPhase1>(out thugBossPhase1);
+        TryGetComponent<ThugBossPhase2>(out thugBossPhase2);
+
+        BossHpGUI.instance.ShowGUI();
+        BossHpGUI.instance.SetMaxHp((int)m_maxHealth);
+        SetHPGUI();
+    }
+    public void InitSetting()
+    {
+        m_health = m_maxHealth;
+        SetHPGUI();
+
+        transform.rotation = Quaternion.Euler(0, 0, 180);
+        StopAllCoroutines();
+        m_phase = 1;
+    }
+
     protected override void Move()
     {
     }
@@ -28,17 +51,41 @@ public class ThugBoss : MonsterBase
         if (m_phase == 1)
         {
             //페이즈1
+            thugBossPhase1.enabled = true;
+            thugBossPhase2.enabled = false;
+
         }
         else if (m_phase == 2)
         {
             //페이즈2
+            thugBossPhase1.enabled = false;
+            thugBossPhase2.enabled = true;
         }
+    }
+
+    public override void GetDamage()
+    {
+        base.GetDamage();
+
+        SetHPGUI();
     }
 
     #endregion
     #region PrivateMethod
-    
+    private void SetHPGUI()
+    {
+        BossHpGUI.instance.SetHp((int)m_health);
+    }
 
+    private void OnEnable()
+    {
 
+        BossHpGUI.instance?.ShowGUI();
+    }
+
+    private void OnDisable()
+    {
+        BossHpGUI.instance?.HideGUI();
+    }
     #endregion
 }
